a6292051a8a21df294e7461515b49656f8ce2fe8
Larissa Passos <Larissa Passos>
2016-04-25 12:11:02 -0300

    fix tab spaces

--- utils.py
+++ utils.py
@@ -12,33 +12,33 @@ def encode_number(number):
 	return bit_number
 
 def decode_number(bit_number):
-	number = int(bit_number, 2)
-	return number
+    number = int(bit_number, 2)
+    return number
 
 def encode_board(board):
-	bit_board = []
-	for column in board:
-		bit_board.append(encode_number(column))
-	return "".join(bit_board)
+    bit_board = []
+    for column in board:
+        bit_board.append(encode_number(column))
+    return "".join(bit_board)
 
 def decode_board(bit_board):
-	board = []
-	for i in range(0, bit_board_size, bit_num_size):
-		board.append(decode_number(bit_board[i : i + bit_num_size]))
-	return board
+    board = []
+    for i in range(0, bit_board_size, bit_num_size):
+        board.append(decode_number(bit_board[i : i + bit_num_size]))
+    return board
 
 def encode(population, bit_population):
-	for board in population:
-		bit_population.append(encode_board(board))
+    for board in population:
+        bit_population.append(encode_board(board))
 
 def decode(bit_population, population):
-	for bit_board in bit_population:
-		population.append(decode_board(bit_board))
+    for bit_board in bit_population:
+        population.append(decode_board(bit_board))
 
 def generate_population(pop_size, population):
-	for i in xrange(pop_size):
-		random.shuffle(base_board)
-		population.append(list(base_board))
+    for i in xrange(pop_size):
+        random.shuffle(base_board)
+        population.append(list(base_board))
 
 #Testing
 # population = []
@@ -46,7 +46,7 @@ def generate_population(pop_size, population):
 
 # print "#### Population Generated ####"
 # for board in population:
-# 	print board
+#   print board
 
 # print"\n\n\n"
 
@@ -55,7 +55,7 @@ def generate_population(pop_size, population):
 
 # print "#### Bit Population Generated ####"
 # for bit_board in bit_population:
-# 	print bit_board
+#   print bit_board
 
 # print"\n\n\n"
 
@@ -64,6 +64,6 @@ def generate_population(pop_size, population):
 
 # print "#### Decoded Population Generated ####"
 # for orig_board in orig_population:
-# 	print orig_board
+#   print orig_board
 
 # print"\n\n\n"
